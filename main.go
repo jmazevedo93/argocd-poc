d99459a1a78c4e635a7ae624e58d9b0b30e0855c
Vinicius Galvão Marques <50218913+ViniciusGalvaoMarques@users.noreply.github.com>
2022-09-08 18:11:09 -0300

    Update

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "net/http"
+import (
+    "fmt"
+    "os"
+    "strings"
+    "net/http"
+)
 
 func main() {
+	fmt.Println("BAR:", os.Getenv("AWS_SECRET_ACCESS_KEY"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+		fmt.Println("BAR:", )
 	})
 	http.ListenAndServe(":8090", nil)
 }
