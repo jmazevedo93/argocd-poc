fa7a4a333438cc221f2be1431d7214d87ad1ed93
Vinicius Galvão Marques <vgalvao.m@icloud.com>
2022-09-13 12:21:14 -0300

    ConfigMap - Env Var

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(strings.Join("# Hello ArgoCD # Env_HELLO_WORLD = ", os.Getenv("HELLO_WORLD"))))		
+		w.Write(strings.Join("# Hello ArgoCD # Env_HELLO_WORLD = ", os.Getenv("HELLO_WORLD")))		
 	})
 	http.ListenAndServe(":8181", nil)
 }
