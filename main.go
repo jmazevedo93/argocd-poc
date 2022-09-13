550a95530f19331715e6fce71889730ca20e563c
Vinicius Galvão Marques <vgalvao.m@icloud.com>
2022-09-13 12:18:42 -0300

    PoC With Env Var

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt"
+    "strings"
     "os"
     "net/http"
 )
 
 func main() {	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Println("# Hello ArgoCD # Env_HELLO_WORLD = ", os.Getenv("HELLO_WORLD"))))		
+		w.Write([]byte(strings.Join("# Hello ArgoCD # Env_HELLO_WORLD = ", os.Getenv("HELLO_WORLD"))))		
 	})
 	http.ListenAndServe(":8181", nil)
 }
